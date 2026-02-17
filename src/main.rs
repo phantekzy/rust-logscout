f3aa3eea754a2c72ca0b3ccfd8f383cd0e25a11e
phantekzy <mainilotfi@gmail.com>
2026-02-17 20:00:19 +0100

    Try to read the file. We use .map_err to convert a standard IO error into our custom error.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::fs;
+
 #[derive(Debug)]
 enum AnalysisError {
     FileNotFound,
     EmptyLog,
+}
+
+fn analyze_logs(filename: &str) -> Result<f32, AnalysisError> {
+    let content = fs::read_to_string(filename).map_err(|_| AnalysisError::FileNotFound)?;
 }
 
 fn main() {
